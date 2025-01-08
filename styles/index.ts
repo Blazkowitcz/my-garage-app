60ad8685d2f6ac944a64a7a7383ceda17a03b36a
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-01-08 17:56:55 +0100

    Add item style

---
+++ styles/index.ts
@@ -0,0 +1,1 @@
+export { default as ItemStyle } from '@/styles/item'
---
+++ styles/item.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    item: {
+        backgroundColor: 'white',
+        color: 'white',
+        padding: 5,
+        marginVertical: 5,
+        marginHorizontal: 10,
+        borderRadius: 10
+    },
+    brand: {
+        color: 'white',
+        fontSize: 25,
+    },
+    type: {
+        color: 'white',
+        fontSize: 20,
+    },
+});
